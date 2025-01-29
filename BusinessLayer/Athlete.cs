49367b13b80e197edf8be1fc82cd3adaf1de5786
kostofff <mitkoaz09az@abv.bg>
2025-01-29 12:15:43 +0200

    InitialCreate

---
+++ BusinessLayer/Athlete.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class Athlete:User
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+
+        public Athlete(string firstName, string lastName, int age)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+        }
+    }
+}
---
+++ BusinessLayer/Club.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    public class Club:User
+    {
+        public string Name { get; set; }
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string League { get; set; }
+        public string Website { get; set; }
+
+        public Club(string name,string country,string city,string league,string website) 
+        { 
+         Name = name;
+         Country = country;
+         City = city;
+         League = league;
+         Website = website;
+        }
+    }
+}
