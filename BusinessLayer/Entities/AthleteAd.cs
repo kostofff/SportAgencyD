e561d63c297e3b5f5d1aa2e24446f3e52d6fef2d
kostofff <mitkoaz09az@abv.bg>
2025-03-09 17:18:55 +0200

    translating buttons

--- BusinessLayer/Entities/AthleteAd.cs
+++ BusinessLayer/Entities/AthleteAd.cs
@@ -36,40 +36,48 @@ namespace BusinessLayer.Entities
         [Key]
         public string Id { get; set; } = Guid.NewGuid().ToString();
 
-
+        [Display(Name = "Заглавие")]
         [Required(ErrorMessage = "Title is required!")]
         [StringLength(100, MinimumLength = 12, ErrorMessage = "Title must be between 12 and 100!")]
         public string Title { get; set; }
 
+        [Display(Name = "Спорт")]
         [Required(ErrorMessage = "Sport is required!")]
         public Sports Sport { get; set; }
 
+        [Display(Name = "Позиция")]
         [Required(ErrorMessage = "Position is required!")]
         public Position Position { get; set; }
 
+        [Display(Name = "Държава")]
         [Required(ErrorMessage = "Country is required!")]
         public Country Country { get; set; }
 
+        [Display(Name = "Град")]
         [Required(ErrorMessage = "City is required!")]
         [StringLength(150, MinimumLength = 1, ErrorMessage = "City name must be between 1 and 150!")]
         public string City { get; set; }
 
+        [Display(Name = "Предпочитан крак")]
         [Required(ErrorMessage = "Must choose between left or rigt foot!")]
         public LeftOrRightFoot LeftOrRighFoot { get; set; }
 
+        [Display(Name = "Отбори, в които е играл")]
         [Required(ErrorMessage = "Teams that you played is required!")]
         [StringLength(5000, MinimumLength = 1, ErrorMessage = "Max 5000 symbols")]
         public string TeamsPlayed { get; set; }
 
+        [Display(Name = "Постижения")]
         [StringLength(1000, MinimumLength = 1, ErrorMessage = "Must be between 1 and 5000!")]
         public string Achievements { get; set; }
 
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
-
+        [Display(Name = "Потребител")]
         [ForeignKey("UserId")]
         public string UserId { get; set; }
 
+        [Display(Name = "Потребител")]
         public virtual User? User { get; set; }
 
         #endregion
--- BusinessLayer/Entities/ClubAd.cs
+++ BusinessLayer/Entities/ClubAd.cs
@@ -14,33 +14,44 @@ namespace BusinessLayer.Entities
         [Key]
         public string Id { get; set; } = Guid.NewGuid().ToString();
 
+        [Display(Name = "Заглавие")]
         [Required(ErrorMessage = "Title is required!")]
         [StringLength(100, MinimumLength = 12, ErrorMessage = "Title must be between 12 and 100!")]
         public string Title { get; set; }
 
+        [Display(Name = "Спорт")]
         [Required(ErrorMessage = "Sport is required!")]
         public Sports Sport { get; set; }
 
+        [Display(Name = "Търсена позиция")]
         [Required(ErrorMessage = "Must have searched position!")]
         public Position SearchedPosition { get; set; }
 
+        [Display(Name = "Търсен силен крак")]
         [Required(ErrorMessage = "Must have searched strong foot!")]
         public LeftOrRightFoot SearchedStrongFoot { get; set; }
 
+        [Display(Name = "Минимум години")]
         [Required(ErrorMessage = "Minimum age is required!")]
         [Range(12, 40, ErrorMessage = "Minimum age must be between 12 and 40!")]
         public int MinimumAge { get; set; }
 
+        [Display(Name = "Максимум години")]
         [Required(ErrorMessage = "Maximum age is required!")]
         [Range(13, 41, ErrorMessage = "Maximum age must be between 12 and 41!")]
         public int MaximumAge { get; set; }
 
+        [Display(Name = "Описание")]
         [StringLength(5000, MinimumLength = 20, ErrorMessage = "Description must be between 20 and 5000 words")]
         public string Description { get; set; }
 
+        [Display(Name = "Дата на създаване")]
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
 
+        [Display(Name = "Потребител")]
         public string UserId { get; set; }
+
+        [Display(Name = "Потребител")]
         [ForeignKey("UserId")]
         public virtual User? User { get; set; }
         #endregion
