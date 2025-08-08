a3b93f8075a395e358023f47640f914047510711
Dimitar Kostov <mitkoaz09az@abv.bg>
2025-08-08 14:08:31 +0300

    sdss

--- DataLayer/Repositories/AthleteAdRepository.cs
+++ DataLayer/Repositories/AthleteAdRepository.cs
@@ -14,7 +14,7 @@ namespace DataLayer.Repositories
             this.context = context;
         }
         #region CRUD
-        public async Task CreateAdAsync(AthleteAd item) // Creating ad method
+        public async Task CreateAdAsync(AthleteAd item) // Creating ad meth
         {
             User userFromDb = await context.Users.FindAsync(item.UserId);
             if (userFromDb != null)
